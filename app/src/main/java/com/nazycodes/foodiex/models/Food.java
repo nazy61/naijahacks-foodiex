6aa2908951c1385ada6e57145ea0989c9364c073
MrTech <ogwo.chinaza@yahoo.com>
2019-12-08 13:26:02 +0100

    "initial commit"

---
+++ app/src/main/java/com/nazycodes/foodiex/models/Food.java
@@ -0,0 +1,13 @@
+package com.nazycodes.foodiex.models;
+
+public class Food {
+    final String id;
+    final String title;
+    final double price;
+
+    public Food (String id, String title, double price){
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+}
